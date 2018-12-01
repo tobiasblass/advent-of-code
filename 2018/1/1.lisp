d20386efddace694465afa08ada6b7ca272b16f4
Tobias Blass <tblass@t-blass.de>
2018-12-01 10:47:20 +0100

    Use a hashtable for the already-seen states.
    This speeds up the program by roughly five orders of magnitude.

--- 2018/1/1.lisp
+++ 2018/1/1.lisp
@@ -8,11 +8,13 @@
   (reduce #'+ frequencies))
 
 (defun first-cycle (frequencies)
-  (iter	(for next-freqs = (or (cdr next-freqs) frequencies)) ; move next-freqs in a cir
+  (iter (with seen = (make-hash-table))
+	(for next-freqs = (or (cdr next-freqs) frequencies)) ; move next-freqs in a circle
 	(for f = (car next-freqs))
-	(for seen initially nil then (cons cur seen))
-	(for cur initially 0 then (+ cur f))
-	(finding cur such-that (member cur seen)))))
+	(for state initially 0 then (progn
+				      (setf (gethash state seen) t)
+				      (+ state f)))
+	(finding state such-that (gethash state seen))))
 
 (assert (= (sum-frequencies '(0 1 2 -1)) 2))
 (assert (= (sum-frequencies '()) 0))
@@ -23,12 +25,4 @@
 (assert (= (first-cycle '(-6 3 8 5 -6)) 5))
 (assert (= (first-cycle '(7 7 -2 -7 -4)) 14))
 
-;; TODO there is some optimization potential, I think
-;; * (time (first-cycle (read-frequencies "input")))
-;; Evaluation took:
-;;  28.866 seconds of real time
-;;  28.826103 seconds of total run time (28.825998 user, 0.000105 system)
-;;  99.86% CPU
-;;  74,820,715,906 processor cycles
-;;  2,359,296 bytes consed
 (first-cycle (read-frequencies "input"))
